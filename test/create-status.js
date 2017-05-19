0fb9056ab613e0d9a879362fd723542d439960bd
Jarrod Davis <developer@jarrodldavis.com>
2017-05-18 21:14:28 -0500

    Fix Create Status
    Fix the tests and logic for Create Status to use the `Context` object
    provided by Probot so that the `owner` and `repo` properties are set in
    the request paylod for `repos.createStatus`.

--- test/create-status.js
+++ test/create-status.js
@@ -10,6 +10,15 @@ const githubMock = {
   }
 };
 
+const contextMock = {
+  repo: object => {
+    return Object.assign({}, {
+      owner: 'owner',
+      repo: 'repo'
+    }, object);
+  }
+};
+
 beforeEach(() => {
   expect.spyOn(githubMock.repos, 'createStatus');
 });
@@ -21,35 +30,41 @@ afterEach(() => {
 describe('create-status', () => {
   it('should create a success status when `gpgStatus` is `true`', () => {
     const sha = crypto.createHash('sha1').digest('hex');
-    createStatus(githubMock, sha, true);
+    createStatus(githubMock, contextMock, sha, true);
     expect(githubMock.repos.createStatus).toHaveBeenCalledWith({
       sha,
       context: 'GPG',
       status: 'success',
-      description: 'All commits have a verified GPG signature'
+      description: 'All commits have a verified GPG signature',
+      owner: 'owner',
+      repo: 'repo'
     });
   });
 
   it('should create a failure status when `gpgStatus` is `false`', () => {
     const sha = crypto.createHash('sha1').digest('hex');
-    createStatus(githubMock, sha, false);
+    createStatus(githubMock, contextMock, sha, false);
     expect(githubMock.repos.createStatus).toHaveBeenCalledWith({
       sha,
       context: 'GPG',
       status: 'failure',
       description: 'All commits must have a verified GPG signature',
-      target_url: 'https://help.github.com/articles/about-gpg/' // eslint-disable-line camelcase
+      target_url: 'https://help.github.com/articles/about-gpg/', // eslint-disable-line camelcase
+      owner: 'owner',
+      repo: 'repo'
     });
   });
 
   it('should create a error status when `gpgStatus` is not a boolean', () => {
     const sha = crypto.createHash('sha1').digest('hex');
-    createStatus(githubMock, sha, 'error');
+    createStatus(githubMock, contextMock, sha, 'error');
     expect(githubMock.repos.createStatus).toHaveBeenCalledWith({
       sha,
       context: 'GPG',
       status: 'error',
-      description: 'An error occurred while checking the commit GPG status'
+      description: 'An error occurred while checking the commit GPG status',
+      owner: 'owner',
+      repo: 'repo'
     });
   });
 });
