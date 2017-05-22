62c708b266e6fd00bd38875f054a40485cf311c4
Jarrod Davis <developer@jarrodldavis.com>
2017-05-21 21:09:52 -0500

    Remove plugin template comments

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const handleEvent = require('./lib/handle-event');
 
 module.exports = robot => {
-  // Your plugin code here
   robot.on('pull_request', (event, context) => handleEvent(robot, event, context));
-
-  // For more information on building plugins:
-  // https://github.com/probot/probot/blob/master/docs/plugins.md
-
-  // To get your plugin running against GitHub, see:
-  // https://github.com/probot/probot/blob/master/docs/development.md
 };
