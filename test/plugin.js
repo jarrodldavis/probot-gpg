f863c77daf68e7dea041e8bab340812bfe42cab1
Jarrod Davis <developer@jarrodldavis.com>
2017-05-21 22:22:46 -0500

    Add pending tests for `Plugin`

---
+++ test/plugin.js
@@ -0,0 +1,15 @@
+describe('plugin', () => {
+  it('should load correctly');
+
+  it('should emit finished event when event is handled successfully');
+
+  it('should emit error event when event handler fails');
+
+  it('should throw if loaded more than once');
+
+  it('should throw if events are given before loading');
+
+  it('should throw if `load` is called with incorrect execution context');
+
+  it('should throw if `acceptEvent` is called with incorrect execution context');
+});
