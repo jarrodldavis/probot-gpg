78bffb581916a6a9b3102614f7239cb5ff40179e
Jarrod Davis <developer@jarrodldavis.com>
2017-06-21 21:10:29 -0500

    Update "GitHub Integration" to "GitHub App"
    Rename references to "GitHub Integration" to "GitHub App" to reflect the
    new name for the stable release of GitHub Apps. Note that the file name
    `test/integration.js` is not one of these references, but instead means
    "integration test" in general.

--- test/utils/create-event.js
+++ test/utils/create-event.js
@@ -1,9 +1,9 @@
-const createIntegrationId = require('./create-integration-id');
+const createAppId = require('./create-app-id');
 
 module.exports = (baseSha, headSha) => {
   return {
     payload: {
-      installation: { id: createIntegrationId() },
+      installation: { id: createAppId() },
       pull_request: { // eslint-disable-line camelcase
         base: { sha: baseSha },
         head: { sha: headSha }
