a0e6a5c0084c938bfce77559a0dc9e96660813dd
Jarrod Davis <developer@jarrodldavis.com>
2017-10-02 22:09:21 -0500

    Add `validate-commit` module

---
+++ test/validate-commit.js
@@ -0,0 +1,39 @@
+const assert = require('assertive');
+const validateCommit = require('../lib/validate-commit');
+
+const createCommit = require('./utils/create-commit');
+
+describe('validate-commit', () => {
+  it('should return "success" if commit is verified', () => {
+    // Arrange
+    const commit = createCommit(true).commit;
+
+    // Act
+    const actual = validateCommit(commit);
+
+    // Assert
+    assert.equal('success', actual);
+  });
+
+  it('should return "failure" if commit is not verified', () => {
+    // Arrange
+    const commit = createCommit(false).commit;
+
+    // Act
+    const actual = validateCommit(commit);
+
+    // Assert
+    assert.equal('failure', actual);
+  });
+
+  it('should return "error" if commit has not verification information', () => {
+    // Arrange
+    const commit = createCommit('error').commit;
+
+    // Act
+    const actual = validateCommit(commit);
+
+    //
+    assert.equal('error', actual);
+  });
+});
