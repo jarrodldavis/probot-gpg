2f7166425a465f08b783fd64e143a658e95626df
Jarrod Davis <developer@jarrodldavis.com>
2017-11-03 21:46:12 -0500

    Use bunyan directly
    Instead of using the wrapped logger from `Robot`, use `bunyan` directly
    in the plugin and event context. This allows for more structured logging
    instead of relying on consistent log formats and prefixes.

--- test/mocks/robot.js
+++ test/mocks/robot.js
@@ -1,10 +1,4 @@
-const createLogStubs = require('../utils/create-log-stubs')
-
 module.exports = class RobotMock {
-  constructor () {
-    this.log = createLogStubs()
-  }
-
   on () {
     return this
   }
